b4d0eff26685b7d5d57f8b8aa234d59b60da97bb
Devin Don <I.INF@Outlook.com>
2020-10-27 20:30:20 +0800

    init: init project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@material-ui/core'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button>Hello, world!</Button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
