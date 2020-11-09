06f6b6511b4df4e5457d0e138d786322ebc79d26
Jiuzhangsuanfa <noreply@jiuzhang.com>
2020-11-09 15:24:34 +0800

    style(actions/accounts): remove console.log

--- src/actions/accounts.ts
+++ src/actions/accounts.ts
@@ -4,7 +4,6 @@ import { AccountType } from '../reducers/accounts';
 export function loginAction(data: ParamsLogin) {
   return async (dispatch: any) => {
     const result = await login(data);
-    console.log({ result })
     dispatch({ type: AccountType.Login, payload: result });
   }
 }
